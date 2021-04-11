7c6f40d2b1772479e780af0f5d39384574eeeb21
Jessele Duran <jessele.duran@rappi.com>
2021-04-11 17:21:37 -0500

    feature: json tags to response

--- coordinates/coordinates.go
+++ coordinates/coordinates.go
@@ -10,7 +10,8 @@ import (
 )
 
 type Coordinates struct {
-  Lat, Lng float64
+  Lat float64 `json:"latitude"`
+  Lng float64 `json:"longitude"`
 }
 
 func Distance(a, b Coordinates) float64 {
--- graph/shortest_path/response.go
+++ graph/shortest_path/response.go
@@ -3,17 +3,17 @@ package shortest_path
 import "github.com/JesseleDuran/osm-graph/coordinates"
 
 type Response struct {
-  Steps       Steps
-  TotalWeight float64 //might be distance
-  Polyline    [][2]float64
+  Steps       Steps        `json:"steps"`
+  TotalWeight float64      //might be distance
+  Polyline    [][2]float64 `json:"polyline"`
 }
 
 type Step struct {
   Weight        float64
-  StartAddress  string
-  EndAddress    string
-  StartLocation coordinates.Coordinates
-  EndLocation   coordinates.Coordinates
+  StartAddress  string                  `json:"start_address"`
+  EndAddress    string                  `json:"end_address"`
+  StartLocation coordinates.Coordinates `json:"start_location"`
+  EndLocation   coordinates.Coordinates `json:"end_location"`
 }
 
 type Steps []Step
